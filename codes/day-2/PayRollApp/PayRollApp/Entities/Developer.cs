62909454050eb79f1a32ccb2cddb5d39e746d03d
Joydip <joydip.mondal@gmail.com>
2025-03-18 12:57:21 +0530

    day-2 final upload

---
+++ codes/day-2/PayRollApp/PayRollApp/Entities/Developer.cs
@@ -0,0 +1,68 @@
+﻿namespace PayRollApp.Entities
+{
+    public class Developer
+    {
+        #region Data Members
+        readonly int id;
+        string name;
+        decimal basicPay;
+        decimal daPay;
+        decimal hraPay;
+        decimal incentivePay;
+        decimal totalPay;
+        #endregion
+
+        #region Constructors
+        public Developer() { }
+
+        public Developer(int id, string name, decimal basicPay, decimal daPay, decimal hraPay, decimal incentivePay)
+        {
+            this.id = id;
+            //this.Name = name;
+            this.name = name;
+            this.basicPay = basicPay;
+            this.daPay = daPay;
+            this.hraPay = hraPay;
+            this.incentivePay = incentivePay;
+        }
+        #endregion
+
+        #region Properties
+        public int Id => id;
+        public decimal TotalPay { get => totalPay; }
+
+        public string Name
+        {
+            set => name = value;
+            get => name;
+        }
+        public decimal BasicPay
+        {
+            get => basicPay;
+            set => basicPay = value;
+        }
+        public decimal DaPay
+        {
+            get => daPay;
+            set => daPay = value;
+        }
+        public decimal HraPay
+        {
+            get => hraPay;
+            set => hraPay = value;
+        }
+        public decimal IncentivePay
+        {
+            get => incentivePay;
+            set => incentivePay = value;
+        }
+        #endregion
+
+        #region Methods
+        public void CalculateSalary()
+        {
+            totalPay = basicPay + daPay + hraPay + incentivePay;
+        }
+        #endregion
+    }
+}
---
+++ codes/day-2/PayRollApp/PayRollApp/Program.cs
@@ -0,0 +1,62 @@
+﻿using PayRollApp.Entities;
+
+namespace PayRollApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //int x = 10;
+            //int y = 20;
+            //int c = x > y ? x : y;
+
+            //string? str = null;
+            //string value = str != null ? str : "";
+            //?? -> null coalesce operator (used only with reference type
+            //string value = str ?? "";
+
+            Developer? developer = Create();
+            developer?.CalculateSalary();
+            Console.WriteLine($"Total salary of {developer?.Name} is {developer?.TotalPay}");
+        }
+        static Developer? Create()
+        {
+            Console.Write("enter name: ");
+            string name = Console.ReadLine() ?? "";
+
+            Random rand = new Random();
+            int id = rand.Next(100, 500);
+
+            Console.Write("enter basic payment: ");
+            //decimal basic = decimal.Parse(Console.ReadLine() ?? "0");
+            decimal basic;
+            decimal.TryParse(Console.ReadLine() ?? "0", out basic);
+
+            Console.Write("enter da payment: ");
+            //decimal da;
+            decimal.TryParse(Console.ReadLine() ?? "0", out decimal da);
+
+            Console.Write("enter hra payment: ");
+            decimal hra;
+            decimal.TryParse(Console.ReadLine() ?? "0", out hra);
+
+            Console.Write("enter incentive payment: ");
+            //decimal incentive = decimal.Parse(Console.ReadLine() ?? "0");
+            decimal.TryParse(Console.ReadLine() ?? "0", out decimal incentive);
+
+            //Developer developer = new Developer();
+            //Developer developer = new();
+
+            Developer developer = new(
+                name: name,
+                id: id,
+                basicPay: basic,
+                daPay: da,
+                hraPay: hra,
+                incentivePay: incentive
+                );
+
+            return developer;
+        }
+    }
+}
