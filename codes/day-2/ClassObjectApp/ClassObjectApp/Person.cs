aa9ee3c598daf035effab37c19852bcfd023c5db
Joydip <joydip.mondal@gmail.com>
2025-03-18 10:46:20 +0530

    class, obejct upload

---
+++ codes/day-2/ClassObjectApp/ClassObjectApp/Person.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassObjectApp
+{
+    class Person
+    {
+        //data members (characterestics)
+        string name;
+        string location;
+        long mobileNo;
+        DateTime? dateOfBirth;
+        //allows this data member to be set ONLY using CONSTRUCTOR
+        readonly int id;
+        //const members MUST be assigned directly while declaring them
+        public const string COMPANY = "Epsilon";
+
+        //default ctor
+        public Person() { }
+
+        //parameterized ctor
+        //Nullable<DateTime> dateOfBirth => optional argument with default value
+        //optional arguments should be the last ones in the method's argument list
+        public Person(string name, int id, string location = "", long mobileNo = 0, Nullable<DateTime> dateOfBirth = null)
+        {
+            this.name = name;
+            this.id = id;
+            this.location = location;
+            this.mobileNo = mobileNo;
+            this.dateOfBirth = dateOfBirth;
+        }
+
+        //Expression Body syntax
+        //public string GetName() => name;
+        //public void SetName(string name) => this.name = name;
+
+        //read-only property
+        //public int Id
+        //{
+        //    get => id;
+        //    //set => id = value;
+        //}
+
+        //read-only property with expression body syntax
+        public int Id => id;
+
+        public string Name
+        {
+            set => name = value;
+            get => name;
+        }
+
+        public long MobileNo
+        {
+            set => mobileNo = value;
+            get => mobileNo;
+        }
+
+        public DateTime? DateOfBirth
+        {
+            set => dateOfBirth = value;
+            get => dateOfBirth;
+        }
+        
+        //property
+        public string Location
+        {
+            //public string GetLocation() => location;
+            
+            //getter or get accessor
+            get => location;
+
+            //public void SetLocation(string location) => this.location = location;
+            
+            //setter or set accessor
+            set => this.location = value;
+        }
+
+        public string PrintInformation()
+        {
+            //string interpolation
+            return $"Name={this.name}, From={location}, Mobile={mobileNo}, Date Of Birth={dateOfBirth}";
+        }
+    }
+}
---
+++ codes/day-2/ClassObjectApp/ClassObjectApp/Program.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualBasic;
+
+namespace ClassObjectApp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            //Person anilPersonRef = new Person();
+            Console.Write("enter name: ");
+            string personName = Console.ReadLine();
+
+            Console.Write("enter id: ");
+            int personId = int.Parse(Console.ReadLine());
+
+            Console.Write("enter location: ");
+            string personLocation = Console.ReadLine();
+
+            Console.Write("enter mobile no: ");
+            long personMobile = long.Parse(Console.ReadLine());
+
+            Console.Write("enter date of birth in MM/DD/YYYY format: ");
+
+            DateTime personDateOfBirth;
+            //it will try to convert the string entered by user in the terminal and returned by the ReadLine() method and then if possible to convert the value to proper date time, that converted value will be assigned to the variable passed by out. if the conversion is NOT possible, then a default value will be assigned to that variable passed by out
+            bool isPossible = DateTime.TryParse(
+               Console.ReadLine(),
+               out personDateOfBirth
+               );
+            Person? anilPersonRef = null;
+            if (isPossible)
+            {
+                //anilPersonRef = new Person(personName, personLocation, personMobile, personDateOfBirth);
+                //named arguments feature (passing the value of the argument with the name of the argument. it helps you to pass values in random order)
+                anilPersonRef = new Person(
+                    mobileNo: personMobile,
+                    id: personId,
+                    name: personName,
+                    dateOfBirth: personDateOfBirth,
+                    location: personLocation
+                    );
+            }
+            else
+            {
+                Console.WriteLine("could not conver the value in proper date of birth...try providing value in correct format...");
+                //anilPersonRef = new Person(personName,personId, personLocation, personMobile);
+            }
+
+            //if (anilPersonRef != null)
+            //{
+            //    string information = anilPersonRef.PrintInformation();
+            //    Console.WriteLine(information);
+            //}
+
+            if (anilPersonRef != null)
+            {
+                anilPersonRef.Location = "Chennai";
+                anilPersonRef.Name = "Anil Kumar";
+                anilPersonRef.DateOfBirth = new DateTime(2000, 1, 13);
+                anilPersonRef.MobileNo = 9090909091;
+            }
+
+            Console.WriteLine(anilPersonRef?.PrintInformation());
+
+            Console.WriteLine($"Name: {anilPersonRef?.Name}");
+            Console.WriteLine($"Location: {anilPersonRef?.Location}");
+
+            //int a = 0;
+            //int b = 0;
+            //int c;
+            //Swap(a, ref b, out c);
+            //Console.WriteLine($"a={a}, b={b}, c={c}");
+        }
+        static void Swap(int x, ref int y, out int z)
+        {
+            x = 10;
+            y = 20;
+            //in this method the variable z (which stores the address of c, MUST BE assigned)
+            z = 30;
+            Console.WriteLine($"x={x}, y={y}, z={z}");
+        }
+    }
+}
