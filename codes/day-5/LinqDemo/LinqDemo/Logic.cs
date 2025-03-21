0c6d09724dcfeee78632bd03cb538a11ef0d738f
Joydip <joydip.mondal@gmail.com>
2025-03-21 15:53:23 +0530

    day-5 final upload

---
+++ codes/day-5/LinqDemo/LinqDemo/Logic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqDemo
+{
+    //declare the delegate whose signature should match to that of the method that you want to invoke
+    delegate bool LogicDel(int a);
+
+    class Logic
+    {
+        public bool IsEven(int x) => x % 2 == 0;
+        public static bool IsOdd(int x) => x % 2 != 0;
+    }
+}
---
+++ codes/day-5/LinqDemo/LinqDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace LinqDemo
+{
+    internal class Program
+    {
+        static List<int> Filter(List<int> list, LogicDel logicDel)
+        {
+            List<int> result = [];
+            foreach (int item in list)
+            {                
+                if (logicDel(item))
+                {
+                    result.Add(item);
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            List<int> list = [1, 2, 3, 4, 5];
+            Logic logic = new();
+            LogicDel evenDel = new LogicDel(logic.IsEven);
+            LogicDel oddDel = new LogicDel(Logic.IsOdd);
+            //Filter(list, evenDel);
+            Filter(list, oddDel);
+
+
+
+            //bool status = evenDel(14);
+            //Console.WriteLine(status);
+        }
+    }
+}
