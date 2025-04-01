9eb39238a610726cb756e220906e993b70228b7d
Joydip <joydip.mondal@gmail.com>
2025-04-01 10:25:56 +0530

    mvc web app

---
+++ codes/day-9/ContactManagementSystem/ContactManagementSystem/Controllers/ContactController.cs
@@ -0,0 +1,54 @@
+﻿using ContactManagementSystem.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ContactManagementSystem.Controllers
+{
+    public class ContactController : Controller
+    {
+        private readonly ILogger<ContactController> logger;
+
+        public ContactController(ILogger<ContactController> logger)
+        {
+            this.logger = logger;
+        }
+
+        //public IActionResult Index(string value)
+        // {
+        //    logger.LogInformation($"received name: {value}");
+        //    this.ViewData["name"] = value;
+        //    return View();
+        //}
+
+        //public IActionResult Index([FromRoute(Name ="value")]string name)
+        //{
+        //    logger.LogInformation($"received name: {name}");
+        //    this.ViewData["name"] = name;
+        //    return View();
+        //}
+
+        //public IActionResult Index([FromRoute(Name = "value")] string name)
+        //{
+        //    logger.LogInformation($"received name: {name}");
+        //    this.ViewBag.UserName = name;
+        //    return View();
+        //}
+
+        public IActionResult Index([FromRoute(Name = "value")] string name)
+        {
+            logger.LogInformation($"received name: {name}");
+            this.ViewData["name"] = name;
+            this.ViewBag.UserName = name;
+
+            //this data will stored in a viewdatadictionary object actually
+            //return View(nameof(Index), name);
+            return this.View(new List<string> { "anil", "sunil" });
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ codes/day-9/ContactManagementSystem/ContactManagementSystem/Program.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var container = builder.Services;
+container.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Contact/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    "default-app-route",
+    "{controller}/{action}/{value?}",
+    new { controller = "Contact", action = "Index" });
+
+app.Run();
